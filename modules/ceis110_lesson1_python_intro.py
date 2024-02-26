fd0bcefa87a9a4741321903dd1e08772b0e7f1e6
Oscar Trevizo <oscar.trevizo@nokia.com>
2024-02-26 17:38:45 -0600

    Coding in Python
    Variables and math intro (Jupyter Notebook and Spyder)

---
+++ modules/ceis110_lesson1_python_intro.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CEIS110 Module 1 - Intro to Python
+# 
+# Author: Prof. Oscar Trevizo
+# 
+# Date: February 26, 2024
+
+
+
+# In[1]:
+
+
+# Declaring and storing values in variables
+
+# Declares a string
+apples_name = 'apple'
+
+# Declares an int
+apples_count = 10
+
+# Declares a float
+apples_price = 0.75
+
+
+print(apples_name, apples_count, apples_price)
+
+
+# In[2]:
+
+
+apples_name
+
+
+# In[3]:
+
+
+apples_count
+
+
+# In[4]:
+
+
+apples_price
+
+
+# In[5]:
+
+
+# Multiplication
+values_of_apples = apples_count * apples_price
+
+print('The value of my apples is = ', values_of_apples, 'dollars')
+
+
+# In[6]:
+
+
+values_of_apples
+
+
+# In[7]:
+
+
+# now let's get some oranges
+oranges_name = 'Orange'
+oranges_count = 20
+oranges_price = 0.5
+
+print(oranges_name, oranges_count, oranges_price)
+
+
+# In[8]:
+
+
+# Addition
+tot_fruit = apples_count + oranges_count
+
+print('The total number of fruit, apples and oranges is =', tot_fruit)
+
+
+# In[9]:
+
+
+apples_count + oranges_count
+
+
+# In[10]:
+
+
+tot_fruit
+
+
+# In[11]:
+
+
+# Subtraction - Eat some apples
+apples_count =  apples_count - 3
+
+print('I ate some apples, so my total now is =', apples_count)
+
+
+# # References:
+# 
+# https://docs.python.org/3/
+# 
+# https://github.com/otrevizo/CEIS110_Python_Jul2023
+# 
+# https://github.com/jakevdp/WhirlwindTourOfPython
+# 
+# https://www.geeksforgeeks.org/python-keywords-and-identifiers/
+# 
+
+# In[ ]:
+
+
+
+
